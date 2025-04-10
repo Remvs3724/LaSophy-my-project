60fdcba4e90b9073d1dfff3eb7e761cd7960b69e
Eli <remvs3724@gmail.com>
2025-04-10 23:33:46 +1000

    update the setting feature

--- src/SettingContent.js
+++ src/SettingContent.js
@@ -1,7 +1,28 @@
 import {useEffect} from "react";
+import { Layout, Menu} from "antd";
+const {Sider, Content}=Layout
 
 
 const SettingsContent=()=>{
+
+const handleMenuClick=()=>{
+
+
+}
+    return (
+        <Menu mode="inline"
+        onClick={handleMenuClick}
+        items={[
+            {key:"account", label:"My account"},
+            {key:"style", label:"style model"}
+
+
+        ]
+        }
+        />
+
+
+    )
 
 }
 export default SettingsContent
